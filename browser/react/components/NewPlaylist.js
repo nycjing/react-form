9af43eeb79c188a9da566ada73eff751a39edf1b
Jess Freasier <jfreasier@gmail.com>
2017-08-23 17:34:38 -0500

    issues with New Playlist State step

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -3,54 +3,42 @@ import { Route, Switch, Link, Redirect } from 'react-router-dom';
 // import axios from 'axios';
 
 
-class NewPlaylist extends React.Component {
+export default class NewPlaylist extends React.Component {
 
     constructor () {
         super();
         this.state = {
-            input: ''
+            inputValue: ''
         };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit (event) {
-        console.log('hd');
-        this.setState ({
-            input: event.target.value
+        event.preventDefault()
+        console.log(event);
+        this.setState({
+            inputValue: event.target.value
         })
+        //console.log(this.state.inputValue)
+
     }
-    //
-    // componentDidMount () {
-    //     const artistId = this.props.match.params.artistId;
-    //     const mainPath = `/api/artists/${artistId}`;
-    //     const paths = [mainPath, `${mainPath}/albums`, `${mainPath}/songs`];
-    //     Promise
-    //         .all(paths.map(path => axios.get(path)))
-    //         .then(responses => responses.map(res => res.data))
-    //         .then(([artist, albums, songs]) => {
-    //             artist.albums = albums;
-    //             artist.songs = songs;
-    //             this.setState({ artist });
-    //         });
-    // }
-    //
+
     render () {
-        console.log('helloe world');
 
         return (
             <div className="well">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={this.handleSubmit}>
                     <fieldset>
                         <legend>New Playlist</legend>
                         <div className="form-group">
                             <label className="col-xs-2 control-label">Name</label>
                             <div className="col-xs-10">
-                                <input className="form-control" type="text"/>
+                                <input className="form-control" type="text" />
                             </div>
                         </div>
                         <div className="form-group">
                             <div className="col-xs-10 col-xs-offset-2">
-                                <button type="submit" className="btn btn-success" onSubmit = {this.handleSubmit}>Create Playlist</button>
+                                <button type="submit" className="btn btn-success">Create Playlist</button>
                             </div>
                         </div>
                     </fieldset>
@@ -60,3 +48,4 @@ class NewPlaylist extends React.Component {
     }
 
 }
+
