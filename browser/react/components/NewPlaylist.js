719cb50bebc1042f8ef367c6181621f286ca1c1d
Jess Freasier <jfreasier@gmail.com>
2017-08-24 10:06:35 -0500

    added warning

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -9,7 +9,8 @@ export default class NewPlaylist extends React.Component {
         super();
         this.state = {
             inputValue: '',
-            flag : false
+            flag: true,
+            warning: null
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,15 +18,23 @@ export default class NewPlaylist extends React.Component {
 
     handleChange(event) {
         event.preventDefault()
-        console.log(event.target.value, this.state.inputValue, this.state.flag);
+        //console.log(event.target.value, this.state.inputValue, this.state.flag);
         this.setState({inputValue: event.target.value});
-        if (this.state.inputvalue && this.state.inputvalue.length >16)  this.setState({flag: true});
+        if (this.state.inputValue && this.state.inputValue.length < 16)  {
+            this.setState({flag: false})
+        } else if (this.state.inputValue && this.state.inputValue.length >= 16) {
+            this.setState({flag: true, warning: "Too many characters"})
+        } else {
+            this.setState({flag: true, warning: "Please enter a name"})
+        }
+        console.log(this.state.inputValue.length)
+
     }
 
     handleSubmit (event) {
         event.preventDefault()
-        console.log('submitbutton',this.state.inputValue);
-        this.setState({inputValue: ''});
+        //console.log('submitbutton',this.state.inputValue);
+        this.setState({inputValue: '', flag: true});
         //console.log(this.state.inputValue)
 
     }
@@ -38,6 +47,12 @@ export default class NewPlaylist extends React.Component {
                     <fieldset>
                         <legend>New Playlist</legend>
                         <div className="form-group">
+                            {
+                                if (this.state.warning) {
+                                    <div className="alert alert-warning">{this.state.warning}</div>
+                                }
+                            }
+
                             <label className="col-xs-2 control-label">Name</label>
                             <div className="col-xs-10">
                                 <input className="form-control" type="text" value={this.state.inputValue} onChange={this.handleChange} />
