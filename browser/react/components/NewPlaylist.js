2919052e6ec9211dd951044dfd822818d33544fe
Jing Jia <nycjing@gmail.com>
2017-08-24 10:36:05 -0400

    validation the input

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -8,17 +8,24 @@ export default class NewPlaylist extends React.Component {
     constructor () {
         super();
         this.state = {
-            inputValue: ''
+            inputValue: '',
+            flag : false
         };
+        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        event.preventDefault()
+        console.log(event.target.value, this.state.inputValue, this.state.flag);
+        this.setState({inputValue: event.target.value});
+        if (this.state.inputvalue && this.state.inputvalue.length >16)  this.setState({flag: true});
     }
 
     handleSubmit (event) {
         event.preventDefault()
-        console.log(event);
-        this.setState({
-            inputValue: event.target.value
-        })
+        console.log('submitbutton',this.state.inputValue);
+        this.setState({inputValue: ''});
         //console.log(this.state.inputValue)
 
     }
@@ -33,12 +40,12 @@ export default class NewPlaylist extends React.Component {
                         <div className="form-group">
                             <label className="col-xs-2 control-label">Name</label>
                             <div className="col-xs-10">
-                                <input className="form-control" type="text" />
+                                <input className="form-control" type="text" value={this.state.inputValue} onChange={this.handleChange} />
                             </div>
                         </div>
                         <div className="form-group">
                             <div className="col-xs-10 col-xs-offset-2">
-                                <button type="submit" className="btn btn-success">Create Playlist</button>
+                                <button type="submit" className="btn btn-success" disabled={this.state.flag}>Create Playlist</button>
                             </div>
                         </div>
                     </fieldset>
